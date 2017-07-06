198a26b425032606a1d63544184fb038db69b9f2
Yan Christoffer <yandepaula@gmail.com>
2017-07-06 13:48:14 -0300

    update modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,18 @@ import {AuthGuard} from "./auth/auth.guard";
 import {ArticleService} from "./services/articles.service";
 import {UserService} from "./services/user.service";
 import { AppComponent } from './app.component';
+import { FormsModule }  from '@angular/forms'
 import{Http,HttpModule} from '@angular/http';
 import { ArticlesComponent } from './articles/articles.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ArticlesComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    HttpModule
+    HttpModule,
+	FormsModule
   ],
   providers: [ArticleService, UserService, AuthGuard, AuthenticationService],
   bootstrap: [AppComponent]
